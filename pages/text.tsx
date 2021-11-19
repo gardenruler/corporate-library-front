f355dcab79d209ac8cea0689d24fdaa44fe3c61d
hyunmyungJaneLee <hmlee2356@gmail.com>
2021-11-20 00:57:58 +0900

    recoil 적용

---
+++ pages/text.tsx
@@ -0,0 +1,25 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { textState } from '../recoil/atoms';
+import { charCountState } from '../recoil/selectors';
+
+function Text() {
+  const [text, setText] = useRecoilState(textState);
+  const count = useRecoilValue(charCountState);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Recoil Test page입니다.</h1>
+      <input type='text' value={text} onChange={onChange} />
+      <p>
+        입력 텍스트: {text} <br/>
+        입력 텍스트 길이: {count}
+      </p>
+    </div>
+  );
+}
+
+export default Text;
