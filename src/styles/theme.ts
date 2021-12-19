46116f3a57c12f2915b5c9a4ebb5bc3411f7b55e
thehue <gouk32@naver.com>
2021-12-19 17:14:32 +0900

    피드백 반영
    - eslintrc.json 개행추가
    - recoil폴터 eslint 에러 수정
    - 필요 없는 타입 제거
    - rem으로 수정
    - package.json lint 스크립트 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,9 @@
 export const size = {
-  largest: '75em', // 1200px
-  large: '56.25em', // 900px
-  medium: '37.5em', // 600px
-  small: '31.25em', // 500px
-  smallest: '25em', // 400px
+  largest: '75rem', // 1200px
+  large: '56.25rem', // 900px
+  medium: '37.5rem', // 600px
+  small: '31.25rem', // 500px
+  smallest: '25rem', // 400px
 };
 
 const theme = {
