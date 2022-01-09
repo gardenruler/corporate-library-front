b131b9d08110087e09847dab9dc7e1e5a772cef2
onethegarden <onethegarden@gmail.com>
2022-01-09 19:49:37 +0900

    jest, tsconfig 경로 수정, 적용, route 모킹 추가

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,14 +1,25 @@
+import { ThemeProvider } from '@emotion/react';
+import theme from '@styles/theme';
 import { render, screen } from '@testing-library/react';
+
 import Home from '../index';
 
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+useRouter.mockImplementation(() => ({
+  pathname: '/',
+}));
+
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />);
+  it('renders a navigation title', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>,
+    );
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    });
+    const title = screen.getByText(/okbut/i);
 
-    expect(heading).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
   });
 });
